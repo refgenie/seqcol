fe0d07cc21aaa50365f2e403ad972c141a6145f3
nsheff <nsheff@users.noreply.github.com>
2019-10-11 12:33:08 -0400

    add html, some object cleanup

--- refget/__init__.py
+++ refget/__init__.py
@@ -4,6 +4,7 @@ import logging
 from ._version import __version__
 from .hash_functions import *
 from .refget import RefDB, RedisDict
+from .refget import fasta_checksum, parse_fasta
 
 __classes__ = ["RefDB", "RedisDict"]
 __all__ = __classes__ + []
--- refget/refget.py
+++ refget/refget.py
@@ -89,19 +89,6 @@ class RefDB(object):
         return checksum
 
 
-    def fasta_checksum(self, fa_file):
-        """
-        Just calculate checksum of fasta file without loading it.
-        """
-        fa_object = pyfaidx.Fasta(fa_file)
-        content_checksums = {}
-        for k in fa_object.keys():
-            content_checksums[k] = self.checksum_function(str(fa_object[k]))
-        collection_string = ";".join([":".join(i) for i in content_checksums.items()])
-        collection_checksum = self.load_seq(collection_string)
-        return collection_checksum, content_checksums
-
-
     def load_fasta(self, fa_file, checksum_function=None):
         """
         Calculates checksums and loads each sequence in a fasta file into the
@@ -110,7 +97,7 @@ class RefDB(object):
         """
         if not checksum_function:
             checksum_function = self.checksum_function        
-        fa_object = pyfaidx.Fasta(fa_file)
+        fa_object = parse_fasta(fa_file)
         content_checksums = {}
         for k in fa_object.keys():
             content_checksums[k] = self.load_seq(str(fa_object[k]))
@@ -150,3 +137,33 @@ class RefDB(object):
             res = "No sequences shared"
 
         return res
+
+
+# Static functions below (these don't require a database)
+
+def parse_fasta(fa_file):
+    _LOGGER.info("Hashing {}".format(fa_file))
+    try:
+        fa_object = pyfaidx.Fasta(fa_file)
+    except pyfaidx.UnsupportedCompressionFormat:
+        # pyfaidx can handle bgzip but not gzip; so we just hack it here and
+        # unzip the file for checksumming, then rezip it for the rest of the
+        # asset build.
+        # TODO: streamline this to avoid repeated compress/decompress
+        os.system("gunzip {}".format(fa_file))
+        fa_file_unzipped = fa_file.replace(".gz", "")
+        fa_object = pyfaidx.Fasta(fa_file_unzipped)
+        os.system("gzip {}".format(fa_file_unzipped))
+    return fa_object
+
+def fasta_checksum(fa_file):
+    """
+    Just calculate checksum of fasta file without loading it.
+    """
+    fa_object = parse_fasta(fa_file)
+    content_checksums = {}
+    for k in fa_object.keys():
+        content_checksums[k] = self.checksum_function(str(fa_object[k]))
+    collection_string = ";".join([":".join(i) for i in content_checksums.items()])
+    collection_checksum = self.load_seq(collection_string)
+    return collection_checksum, content_checksums
