698352ca5b92cab612cb224f168262558e77a916
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-07-31 15:35:31 -0400

    update tests

--- tests/test_seqcol.py
+++ tests/test_seqcol.py
@@ -17,12 +17,12 @@ class TestGeneral:
 
 class TestFastaInserting:
     @pytest.mark.parametrize("fasta_name", DEMO_FILES)
-    def test_insert_works(self, fasta_name, fasta_path):
+    def test_fasta_loading_works(self, fasta_name, fasta_path):
         scc = SeqColClient({})
         f = os.path.join(fasta_path, fasta_name)
         print("Fasta file to be loaded: {}".format(f))
         res = scc.load_fasta(f)
-        assert len(res) == 2  # returns digest and list of ASDs
+        assert len(res) == 2  # returns digest and list of AnnotatedSequencesList
 
 
 class TestRetrieval:
@@ -32,34 +32,6 @@ class TestRetrieval:
         f = os.path.join(fasta_path, fasta_name)
         print("Fasta file to be loaded: {}".format(f))
         d, asds = scc.load_fasta(f)
-        assert scc.retrieve(d, reclimit=0) == \
-               [{k: str(v) for k, v in asd.items()} for asd in asds]
-
-    @pytest.mark.parametrize("fasta_name", DEMO_FILES)
-    def test_refget_works(self, fasta_name, fasta_path):
-        scc = SeqColClient({})
-        f = os.path.join(fasta_path, fasta_name)
-        print("Fasta file to be loaded: {}".format(f))
-        d, asds = scc.load_fasta(f)
-        assert scc.refget(d, reclimit=0) == \
-               [{k: str(v) for k, v in asd.items()} for asd in asds]
-
-
-class TestBoth:
-    @pytest.mark.parametrize("fasta_name", DEMO_FILES)
-    def test_roundtrip(self, fasta_name, fasta_path):
-        """
-        Verify that FASTA loaded, retrieved and decoded from RefGetHenge
-        yields identical collection digest after loading
-        """
-        rgdb = SeqColClient({})
-        f = os.path.join(fasta_path, fasta_name)
-        print("Fasta file to be loaded: {}".format(f))
-        d, asds = rgdb.load_fasta(f)
-        _, path = tempfile.mkstemp(".fa")
-        with open(path, "w") as fh:
-            fh.write(rgdb.fasta_fmt(rgdb.refget(d)))
-        d1, asds1 = rgdb.load_fasta(path)
-        assert d1 == d
-        os.remove(path)
-
+        # convert integers in the dicts to strings
+        lst = [{k: str(v) if isinstance(v, int) else v for k, v in asd.items()} for asd in asds]
+        assert scc.retrieve(d) == lst
