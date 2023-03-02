46675b669ae07db9da4fc3d113fefa2c1667b1fb
nsheff <nsheff@users.noreply.github.com>
2023-03-02 10:16:50 -0500

    Update to latest henge, polish inherent attrs, names-lengths

--- seqcol/seqcol.py
+++ seqcol/seqcol.py
@@ -134,7 +134,7 @@ class SeqColClient(refget.RefGetClient):
         B_filtered = list(filter(lambda x: x in A, B))
         A_count = len(A_filtered)
         B_count = len(B_filtered)
-        overlap = min(len(A_filtered), len(B_filtered)) ## counts duplicates
+        overlap = min(len(A_filtered), len(B_filtered))  ## counts duplicates
 
         if A_count + B_count < 1:
             # order match requires at least 2 matching elements
@@ -143,8 +143,8 @@ class SeqColClient(refget.RefGetClient):
             # duplicated matches means order match is undefined
             order = None
         else:
-            order = (A_filtered == B_filtered)
-        return { "a-and-b": overlap, "a-and-b-same-order": order }
+            order = A_filtered == B_filtered
+        return {"a-and-b": overlap, "a-and-b-same-order": order}
 
     @staticmethod
     def compare(A, B):
@@ -158,19 +158,12 @@ class SeqColClient(refget.RefGetClient):
         all_keys = list(A.keys()) + list(set(B.keys()) - set(list(A.keys())))
         result = {}
         return_obj = {
-            "arrays": {
-                "a-only": [],
-                "b-only": [],
-                "a-and-b": []
-            },
+            "arrays": {"a-only": [], "b-only": [], "a-and-b": []},
             "elements": {
-                "total": {
-                    "a": len(A["lengths"]),
-                    "b": len(B["lengths"])
-                },
+                "total": {"a": len(A["lengths"]), "b": len(B["lengths"])},
                 "a-and-b": {},
                 "a-and-b-same-order": {},
-            }
+            },
         }
 
         for k in all_keys:
@@ -184,10 +177,11 @@ class SeqColClient(refget.RefGetClient):
                 return_obj["arrays"]["a-and-b"].append(k)
                 res = SeqColClient.compare_elements(A[k], B[k])
                 return_obj["elements"]["a-and-b"][k] = res["a-and-b"]
-                return_obj["elements"]["a-and-b-same-order"][k] = res["a-and-b-same-order"]
+                return_obj["elements"]["a-and-b-same-order"][k] = res[
+                    "a-and-b-same-order"
+                ]
         return return_obj
 
- 
     def retrieve(self, druid, reclimit=None, raw=False):
         try:
             return super(SeqColClient, self).retrieve(druid, reclimit, raw)
@@ -202,7 +196,6 @@ class SeqColClient(refget.RefGetClient):
                     "{} not found in database, or in refget.".format(druid)
                 )
 
-
     def load_fasta_from_refgenie(self, rgc, refgenie_key):
         """
         @param rgc RefGenConf object
@@ -212,23 +205,32 @@ class SeqColClient(refget.RefGetClient):
         filepath = rgc.seek(refgenie_key, "fasta")
         return self.load_fasta_from_filepath(filepath)
 
-
     def load_fasta_from_filepath(self, filepath):
         """
         @param filepath Path to fasta file
-        @param sc 
+        @param sc
         """
         fa_object = parse_fasta(filepath)
-        SCAS = fasta_to_scas(fa_object)
-        SCAS["names_lengths"] = build_names_lengths(SCAS, self.checksum_function)
-        _LOGGER.debug(f"names_lengths: {SCAS['names_lengths']}")
+        SCAS = fasta_to_scas(fa_object, digest_function=self.checksum_function)
         digest = self.insert(SCAS, "SeqColArraySet", reclimit=1)
         return {
-          "fa_file": filepath,
-          "fa_object": fa_object,
-          "SCAS": SCAS,
-          "digest": digest
+            "fa_file": filepath,
+            "fa_object": fa_object,
+            "SCAS": SCAS,
+            "digest": digest,
         }
+
+    def load_multiple_fastas(self, fasta_dict):
+        """
+        Wrapper for load_fasta_from_filepath
+
+        @param fasta_list
+        """
+        results = {}
+        for name, path in fasta_dict.items():
+            print(f"Processing fasta '{name}'' at path '{path}'...")
+            results[name] = self.load_fasta_from_filepath(path)
+        return results
 
 
 # Static functions below (these don't require a database)
@@ -238,8 +240,8 @@ def explain_flag(flag):
     """Explains a compare flag"""
     print(f"Flag: {flag}\nBinary: {bin(flag)}\n")
     for e in range(0, 13):
-        if flag & 2 ** e:
-            print(FLAGS[2 ** e])
+        if flag & 2**e:
+            print(FLAGS[2**e])
 
 
 def parse_fasta(fa_file):
@@ -263,38 +265,43 @@ def parse_fasta(fa_file):
             f_out.seek(0)
             return pyfaidx.Fasta(f_out.name)
 
-# static 
-def fasta_to_scas(fa_object, verbose=True):
+
+# static
+def fasta_to_scas(fa_object, verbose=True, digest_function=henge.md5):
     """
-    Given a fasta object, return a SCAS (Sequence Colletion Array Set)
+    Given a fasta object, return a SCAS (Sequence Collection Array Set)
     """
     # SCAS = SeqColArraySet
     # Or maybe should be "Level 1 SC"
-    SCAS = {"lengths": [] , "names": [], "sequences": []}
+    SCAS = {"lengths": [], "names": [], "sequences": [], "names_lengths": []}
     seqs = fa_object.keys()
     nseqs = len(seqs)
     print(f"Found {nseqs} chromosomes")
-    print("Building names_lengths attribute")
-    i=1
+    i = 1
     for k in fa_object.keys():
         if verbose:
             print(f"Processing ({i} of {nseqs}) {k}...")
         seq = str(fa_object[k])
-        digest = henge.md5(seq.upper())
-        SCAS["lengths"].append(str(len(seq)))
-        SCAS["names"].append(fa_object[k].name)
-        SCAS["sequences"].append(digest)
+        seq_length = len(seq)
+        seq_name = fa_object[k].name
+        seq_digest = digest_function(seq.upper())
+        nl = {"length": seq_length, "name": seq_name}
+        nl_digest = digest_function(henge.canonical_str(nl))
+        SCAS["lengths"].append(seq_length)
+        SCAS["names"].append(seq_name)
+        SCAS["names_lengths"].append(nl_digest)
+        SCAS["sequences"].append(seq_digest)
         i += 1
 
+    SCAS["names_lengths"].sort()
     return SCAS
 
+
 def build_names_lengths(obj: dict, digest_function):
-    """ Builds the names_lengths attribute, which corresponds to the coordinate system """
+    """Builds the names_lengths attribute, which corresponds to the coordinate system"""
     names_lengths = []
     for i in range(len(obj["names"])):
-        names_lengths.append(
-            {"length": obj["lengths"][i], 
-                        "name": obj["names"][i]})
+        names_lengths.append({"length": obj["lengths"][i], "name": obj["names"][i]})
     nl_digests = []
     for i in range(len(names_lengths)):
         nl_digests.append(digest_function(henge.canonical_str(names_lengths[i])))
